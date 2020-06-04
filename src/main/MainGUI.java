ef028847b020ee9f9498c798139f57666ca729c7
Killer7502 <sawyerkole@gmail.com>
2020-06-04 13:28:32 -0400

    Created and formated GUI components

--- src/main/MainGUI.java
+++ src/main/MainGUI.java
@@ -1,10 +1,28 @@
 package main;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainGUI {
+	
+	private JFrame frameMain;
+	private JLabel lblWord;
+	private JTextField textWord;
+	private JTextField textAnswer;
+	private JTextArea textOutput;
+	private JButton btnStart;
+	private JButton btnLoad;
+	private JButton btnQuit;
+	private JButton btnCheck;
+	private JLabel lblAnswer;
+	private JFileChooser fileChooser;
 
 	private JFrame frame;
 
@@ -35,9 +53,56 @@ public class MainGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameMain = new JFrame();
+		frameMain.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frameMain.getContentPane().setLayout(null);
+		frameMain.setLocationRelativeTo(null);
+		
+		fileChooser = new JFileChooser();
+		
+		lblWord = new JLabel("Word:");
+		lblWord.setBounds(10, 11, 52, 14);
+		frameMain.getContentPane().add(lblWord);
+		
+		lblAnswer = new JLabel("Answer:");
+		lblAnswer.setBounds(10, 36, 52, 14);
+		frameMain.getContentPane().add(lblAnswer);
+		
+		textWord = new JTextField();
+		textWord.setEditable(false);
+		textWord.setBounds(64, 8, 220, 20);
+		frameMain.getContentPane().add(textWord);
+		textWord.setColumns(10);
+		
+		textAnswer = new JTextField();
+		textAnswer.setBounds(64, 33, 220, 20);
+		frameMain.getContentPane().add(textAnswer);
+		textAnswer.setColumns(10);
+		
+		btnQuit = new JButton("Quit");
+		btnQuit.setBounds(195, 98, 89, 23);
+		frameMain.getContentPane().add(btnQuit);
+		
+		btnLoad = new JButton("Load Data");
+		btnLoad.setBounds(102, 98, 89, 23);
+		frameMain.getContentPane().add(btnLoad);
+		
+		btnStart = new JButton("Start");
+		btnStart.setBounds(10, 98, 89, 23);
+		frameMain.getContentPane().add(btnStart);
+		
+		btnCheck = new JButton("Check");
+		btnCheck.setBounds(195, 64, 89, 23);
+		frameMain.getContentPane().add(btnCheck);
+		
+		textOutput = new JTextArea();
+		textOutput.setEditable(false);
+		textOutput.setBounds(10, 61, 178, 28);
+		frameMain.getContentPane().add(textOutput);
+		frameMain.setTitle("Vocab");
+		frameMain.setResizable(false);
+		frameMain.setBounds(100, 100, 300, 160);
+		frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
